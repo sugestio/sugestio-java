72749a03953fd9c5644986c605610bf507bdf5d6
kvanhecke <kris0476@gmail.com>
2011-12-19 14:36:37 +0100

    filter on tag, location_simple or location_city

--- src/com/sugestio/client/call/RecommendationFilter.java
+++ src/com/sugestio/client/call/RecommendationFilter.java
@@ -1,6 +1,6 @@
 package com.sugestio.client.call;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.core.MultivaluedMap;
@@ -11,12 +11,20 @@ public class RecommendationFilter {
 	
 	private List<String> categories;
 	private List<String> segments;
+	private List<String> tags;
+	private List<String> location_simples;
+	private List<String> location_cities;
 	private Integer limit;
 	
-	
+	/**
+	 * Creates an empty RecommendationFilter
+	 */
 	public RecommendationFilter() {
-		this.categories = new ArrayList<String>();
-		this.segments = new ArrayList<String>();
+		this.categories = new LinkedList<String>();
+		this.segments = new LinkedList<String>();
+		this.tags = new LinkedList<String>();
+		this.location_simples = new LinkedList<String>();
+		this.location_cities = new LinkedList<String>();
 		this.limit = null;
 	}
 	
@@ -45,6 +53,42 @@ public class RecommendationFilter {
 	}
 	
 	/**
+	 * Retrieved items must (not) have this tag
+	 * @param tag
+	 * @param value
+	 */
+	public void hasTag(String tag, boolean value) {
+		if (value)
+			this.tags.add(tag);
+		else
+			this.tags.add("!" + tag);
+	}
+	
+	/**
+	 * Retrieved items must (not) be in this location
+	 * @param location
+	 * @param value
+	 */
+	public void inLocation_simple(String location, boolean value) {
+		if (value)
+			this.location_simples.add(location);
+		else
+			this.location_simples.add("!" + location);
+	}
+	
+	/**
+	 * Retrieved items must (not) be in this location
+	 * @param location
+	 * @param value
+	 */
+	public void inLocation_city(String location, boolean value) {
+		if (value)
+			this.location_cities.add(location);
+		else
+			this.location_cities.add("!" + location);
+	}
+	
+	/**
 	 * Maximum number of recommendations to be retrieved.
 	 * @param limit
 	 */
@@ -62,6 +106,18 @@ public class RecommendationFilter {
 		
 		for (String segment : segments) {
 			params.add("segment[]", segment);
+		}
+		
+		for (String tag : tags) {
+			params.add("tag[]", tag);
+		}
+		
+		for (String location : location_simples) {
+			params.add("location_simple[]", location);
+		}
+		
+		for (String location : location_cities) {
+			params.add("location_city[]", location);
 		}
 		
 		if (limit != null) {
